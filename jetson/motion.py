ad8b9687e4812713f82ccb8e035b08d0c3ff54f3
JBoT21 <bowen.jackson@yahoo.com>
2026-05-21 13:07:10 -0400

    working on motion.py

--- jetson/motion.py
+++ jetson/motion.py
@@ -8,32 +8,22 @@ def MotionController(serial):
     print("MotionController: Not implemented yet")
 
 def forward(speed):
-    return None 
-#Will return a string command to send to the ESP32 (ex: "FORWARD:100")
+    return f"FORWARD:{speed}"
+
 
 def backward(speed):
-    #Will do later
-    #Will return a string command to send to the ESP32 (ex: "BACKWARD:100")
-    return None
+    return f"BACKWARD:{speed}"
 
 def turn_left(speed):
-    #Will do later
-    #Will return a string command to send to the ESP32 (ex: "TURN_LEFT:100")
-    return None
+    return f"TURN_LEFT:{speed}"
 
 def turn_right(speed):
-    #Will do later
-    #Will return a string command to send to the ESP32 (ex: "TURN_RIGHT:100")
-    return None
+    return f"TURN_RIGHT:{speed}"
 
 def stop():
-    #Will do later
-    #Will return a string command to send to the ESP32 (ex: "STOP")
-    return None
+    return "STOP"
 
 def servo(angle):
-    #Will do later
-    #Will return a string command to send to the ESP32 (ex: "SERVO:90")
-    return None
+   return f"SERVO:{angle}"
 
 
