fe371fdebb7be0889f26692a36ba6f813ca52262
JBoT21 <bowen.jackson@yahoo.com>
2026-05-21 12:39:18 -0400

    Biiiig work on main

--- jetson/main.py
+++ jetson/main.py
@@ -45,7 +45,7 @@ def main():
     print("Initializing SLAM components...")
     grid = OccupancyGrid(size = 200)
     localization = Localization(grid)
-    mapping = MappingEngine(grid, localization)
+    mapper = MappingEngine(grid, localization)
     navigator = Navigator(grid, localization)
     visual = Visualizer(grid, localization)  # future development
     print("SLAM components initialized.")
@@ -57,6 +57,49 @@ def main():
     #LoopHZ is the frequency at which the main loop runs (10 Hz = 100 ms per iteration)
     LoopDT = 1.0 / LoopHZ
     #LoopDT is the time delay between iterations (0.1 seconds for 10 Hz)
+
+    try:
+        while True:
+            loop_start = time.time()
+
+            #1. Read sensors
+            raw = serial.read_line()
+            sensor_data = sensors.parse(raw)
+
+            if sensor_data.ultrasonic is not None:
+                mapper.update_from_ultrasonic(sensor_data.ultrasonic)
+            if sensor_data.imu is not None:
+                localization.update_from_imu(sensor_data.imu)
+
+            #2. Update localization
+                localization.predict()
+
+            #3. Update map
+                mapper.integrate()
+
+            #4 Navigation decision
+                command = navigator.decide_next_move()
+                motion.execute(command)
+
+            #5. Visualization (optional)
+                visual.update()
+
+            #6. Keep track of loop timing
+                time_elapsed = time.time() - loop_start
+                if time_elapsed < LoopDT:
+                    time.sleep(LoopDT - time_elapsed)
+                else:
+                    print(f"Warning: Loop took {time_elapsed:.2f} seconds. Desired time is: {LoopDT:.2f} seconds.")
+
+                
+    except KeyboardInterrupt:
+        print("Shutting down SLAM system...")
+        motion.stop()
+        serial.close()
+
+if __name__ == "__main__":
+    main()
+
 
 
 
