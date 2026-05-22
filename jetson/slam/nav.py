fe69fa29521477596feb2b77ebc805d6d952a6fb
JBoT21 <bowen.jackson@yahoo.com>
2026-05-22 15:24:59 -0400

    Made changes

--- jetson/slam/nav.py
+++ jetson/slam/nav.py
@@ -1,18 +1,12 @@
 """
 Responsiblities:
-
 1.High-level decision making:
-
 2.Move forward until obstacle
-
 3. Turn left/right based on map
-
 4.Explore unknown areas
-
 5.Avoid revisiting mapped areas
 
-Later, you can add:
-
+Later, I will add:
 A*,DWA,Q-learning or Frontier exploration (oooooh)
 
 """
