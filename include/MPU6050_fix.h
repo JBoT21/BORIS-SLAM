b444d9b03e5454eb759e92cb13d213816e825dec
JBoT21 <bowen.jackson@yahoo.com>
2026-05-29 15:19:30 -0400

    STILL working on MPU fix

--- include/MPU6050_fix.h
+++ include/MPU6050_fix.h
@@ -1,2 +1,4 @@
 #pragma once
-#include ".pio/libdeps/esp32dev/MPU6050/src/MPU6050.h"
+
+// Force IntelliSense to use the correct ElectronicCats header
+#include "../.pio/libdeps/esp32dev/MPU6050/src/MPU6050.h"
